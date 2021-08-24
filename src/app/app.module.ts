f59c6ccbefabe0e6fb9fb554fd5a55ca8bbbcb3e
anjalishinde1912 <anjalishinde1912@gmail.com>
2021-08-24 13:19:42 +0530

    First push

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TudoComponent } from './tudo/tudo.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TudoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/tudo/tudo.component.ts
@@ -0,0 +1,61 @@
+import { analyzeAndValidateNgModules } from '@angular/compiler';
+import { stringify } from '@angular/compiler/src/util';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from './../../app/model/Todo';
+
+@Component({
+  selector: 'app-tudo',
+  templateUrl: './tudo.component.html',
+  styleUrls: ['./tudo.component.css']
+})
+export class TudoComponent implements OnInit {
+  
+todos: Todo[];
+inputtodo:string="";
+
+  constructor() { 
+    this.todos=[];
+  }
+
+  ngOnInit(): void {
+    this.todos = [
+      {
+        content:'First todo',
+        completed: true
+      },
+
+      {
+        content:'second todo',
+        completed: true
+      }
+     
+    ]
+    this.todos = JSON.parse(localStorage.getItem("todos") || '')  ;
+  }
+   
+  toggledone (id:number) 
+  {
+   this.todos.map((v,i) => {
+   if (i==id) v.completed = !v.completed;
+   return v;
+    })
+  }
+   
+  deleteTodo (id:number) 
+  {
+    let todos = JSON.parse(localStorage.getItem("todos") || '')  ;
+    this.todos=this.todos.filter((v,i)=> i!==id);
+    todos = JSON.parse(localStorage.getItem("todos") || '')  ;
+  }
+
+  addTodo () 
+  {
+    this.todos.push({
+        content: this.inputtodo,
+        completed: false
+    });
+
+    this.inputtodo="";
+    localStorage.setItem("todos",JSON.stringify(this.todos));
+  }
+}
